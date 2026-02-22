2b9d2a2e45c7ae8c86e9ba93b7e72d78ee4f0a72
Marcus Rezende <marcusviniciusxmvr@gmail.com>
2026-02-21 21:35:31 -0300

    Adicionamento da seção contatos e rodape ja estiizadas

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,10 +1,11 @@
-import { FaUpRightAndDownLeftFromCenter } from "react-icons/fa6";
-
+import { FaHeart } from "react-icons/fa";
+import "../styles/footer.css";
 function Footer() {
   return (
-    <div>
-      <h1>ULTIMA PARTE POURAAA</h1>
-    </div>
+    <footer className="footer">
+      <p className=" texto-footer">© 2026 MarcusRezende | Todos os direitos reservados.</p>
+      <p className="texto-footer">Feito com <FaHeart className="icone-footer" /> en Brasil </p>
+    </footer>
   );
 }
 export default Footer;
